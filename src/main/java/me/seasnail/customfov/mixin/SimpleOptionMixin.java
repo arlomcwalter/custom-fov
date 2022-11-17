018760afac0ec128442dda76241b8561ccb14fd6
seasnail <seasnail8169@gmail.com>
2022-11-17 01:57:04 +0000

    remove unnecessary @Redirect mixin

--- src/main/java/me/seasnail/customfov/mixin/SimpleOptionMixin.java
+++ src/main/java/me/seasnail/customfov/mixin/SimpleOptionMixin.java
@@ -1,16 +1,36 @@
 package me.seasnail.customfov.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.SimpleOption;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Optional;
+import java.util.function.Consumer;
 
 @Mixin(SimpleOption.class)
 public class SimpleOptionMixin<T> {
-    @Redirect(method = "setValue", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/SimpleOption$Callbacks;validate(Ljava/lang/Object;)Ljava/util/Optional;"))
-    private Optional<T> bypassValidation(SimpleOption.Callbacks<T> validator, T value) {
-        return Optional.of(value);
+    @Shadow
+    T value;
+
+    @Shadow @Final
+    private Consumer<T> changeCallback;
+
+    @Inject(
+            method = "setValue",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/option/SimpleOption$Callbacks;validate(Ljava/lang/Object;)Ljava/util/Optional;", shift = At.Shift.BEFORE),
+            cancellable = true
+    )
+    private void earlyReturn(T value, CallbackInfo ci) {
+        this.value = value;
+
+        if (MinecraftClient.getInstance().isRunning()) {
+            changeCallback.accept(value);
+        }
+
+        ci.cancel();
     }
 }
